8cf42972cb441fc4f459e20b15f05572562fba74
Alcohol120 <alcohol120ds@gmail.com>
2017-04-24 13:59:58 +0200

    Initial commit

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => "Alcohol120%",
+    "author_url" => "http://alclab.pro",
+    "name" => "MultiLang",
+    "description" => "Multilanguage support",
+    "version" => "0.0.1",
+    "namespace" => "AlcLab\\MultiLang"
+];
